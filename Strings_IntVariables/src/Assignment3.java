e705297f4828e550ea3a36e8ffc6267accae7785
Mosammad Kabir <97767575+m-kabir02@users.noreply.github.com>
2022-10-08 22:37:32 -0400

    Add files via upload

---
+++ Strings_IntVariables/src/Assignment3.java
@@ -0,0 +1,35 @@
+/*
+Mosammad Kabir
+Professor DeBello
+CUS 1115
+Feb 11, 2021
+*/
+public class Assignment3 {
+    public static void main(String[] args)
+    {
+        int x; int y; int z;
+        x=1; y=2; z=6;
+        int sum=x+y+z;
+        System.out.println("The sum of the 3 integers is "+sum);
+
+        int product=x*y*z;
+        System.out.println("The product of the 3 integers is "+product);
+
+        String title="Ms.";
+        String first="Mary";
+        String last="Smith";
+        System.out.println("The person enrolled in the course is. "+last+", "+first+" "+title);
+
+        int a=3;
+        int b=5;
+        int c=9;
+
+        product=(a*c)%b;
+        System.out.println("The product of (a*b)-c is "+product);
+
+
+
+
+
+    }
+}
